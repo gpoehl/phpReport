3274c79c3bc8b0f20837d320ff0cff08b7eb8e5c
gpoehl <g.poehl@gmx.de>
2020-06-28 13:47:05 +0200

    implement collector range method

--- src/AbstractCollector.php
+++ src/AbstractCollector.php
@@ -82,14 +82,14 @@ abstract class AbstractCollector implements \ArrayAccess {
         }
         trigger_error("Item $key does not exit", E_USER_NOTICE);
     }
-    
-   /**
-    * Adds values to related calculators or to other collectors. 
-    * @param iterable $values Key and value pairs. Key represents the collector
-    * item. Value might be numeric value or array having key => value pair(s). 
-    */ 
+
+    /**
+     * Adds values to related calculators or to other collectors. 
+     * @param iterable $values Key and value pairs. Key represents the collector
+     * item. Value might be numeric value or array having key => value pair(s). 
+     */
     public function add(iterable $values) {
-        foreach ($values as $item => $value){ 
+        foreach ($values as $item => $value) {
             $this->items[$item]->add($value);
         }
     }
@@ -98,6 +98,50 @@ abstract class AbstractCollector implements \ArrayAccess {
         foreach ($this->items as $item) {
             $item->cumulateToNextLevel();
         }
+    }
+
+    /**
+     * Select ranges or single items from items.
+     * @param array|int|string $ranges Any number of ranges can be passed. When 
+     * a range is not an array than the single item will be selected. Not existing
+     * single items will be ignored.
+     * For arrays the first element is the start item to be selected and the second element
+     * the last item to be selected. When the second item is null all items starting
+     * from the first item will be selected.
+     * The start item and the last item must exist. 
+     * @return AbstractCollector Collector collecting only selected itemes
+     * * @throws InvalidArgumentException When start or end item doesn't exist.
+     */
+    public function range(... $ranges): AbstractCollector {
+        $result = [];
+        $singleSelects = [];
+        $itemKeys = array_keys($this->items);
+        foreach ($ranges as $range) {
+            if (is_array($range)) {
+                $fromKey = array_search($range[0], $itemKeys, true);
+                if ($fromKey === false) {
+                    throw new InvalidArgumentException("From key $fromKey doesn't exist.");
+                }
+                if (isset($range[1])) {
+                    $toKey = array_search($range[1], $itemKeys, true);
+                    if ($toKey === false) {
+                        throw new InvalidArgumentException("To key $toKey doesn't exist.");
+                    }
+                    $length = $toKey - $fromKey + 1;
+                } else {
+                    $length = null;
+                }
+                $result = $result + array_slice($this->items, $fromKey, $length, true);
+            } else {
+                $singleSelects [] = $range;
+            }
+        }
+        if (!empty($singleSelects)) {
+            $result += array_intersect_key($this->items, array_flip($singleSelects));
+        }
+        $ret = clone($this);
+        $ret->items = $result;
+        return $ret;
     }
 
     public function sum($level = null, bool $asArray = false) {
